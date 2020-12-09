678aa67e5b029c438203023406f566104937cc98
web2maynard <web2.maynard@gmail.com>
2020-12-09 09:00:32 +0800

    Agency List Functionality

--- application/modules/gagencylist/controllers/Gagencylist.php
+++ application/modules/gagencylist/controllers/Gagencylist.php
@@ -54,7 +54,7 @@ class Gagencylist extends MY_Controller {
          }
 
          $valid_columns = array(
-   			1=>'image',
+   			1=>'agency_image',
    			2=>'agency_name',
    		);
 
@@ -100,9 +100,10 @@ class Gagencylist extends MY_Controller {
 
           foreach($agencies->result() as $r) {
 
-                  $action_btn = "<a class='btn btn-primary btn-xs status_user  blue-btn' data-id=".$r->gagency_id."  href='javascript:void(0)'>View <i class='fa fa-eye'></i></a>";
+                  $action_btn = "<a class='btn btn-primary btn-xs status_user view_agency  blue-btn' data-id=".$r->gagency_id."  href='javascript:void(0)'>View <i class='fa fa-eye'></i></a>";
                   if($this->session->userdata('user_details')[0]['user_type'] == 'admin'){
-                     $action_btn .= "<a class='btn btn-success btn-xs status_user' data-id=".$r->gagency_id." href='javascript:void(0)'>Edit <i class='fa fa-pen'></i></a>";
+                     $action_btn .= "<a class='btn btn-success btn-xs status_user edit_agency' data-id=".$r->gagency_id." href='javascript:void(0)'>Edit <i class='fa fa-pen'></i></a>";
+                     $action_btn .= "<a class='btn btn-danger btn-xs status_user delete_agency' href='".base_url('gagencylist/delete_agency/'.$r->gagency_id)."'>Delete <i class='fa fa-trash'></i></a>";
                   }
 
                   $path = base_url('/assets/images/agencies/') . $r->agency_image;
@@ -126,5 +127,100 @@ class Gagencylist extends MY_Controller {
           echo json_encode($output);
           exit();
 	}
+
+  public function add_agency(){
+
+      $data1 = $_POST['imagebase64'];
+
+      if($data1 == 'data:,'){
+         $product_image = 'main-logo.png';
+      }else{
+        list($type, $data1) = explode(';', $data1);
+        list(, $data1)      = explode(',', $data1);
+        $data5 = base64_decode($data1);
+        $imgname = "agency_".substr(md5(uniqid(rand(1,8))), 0, 8).".png";
+        $test = file_put_contents("assets/images/agencies/$imgname", $data5);
+
+        $filename = $filetmpname = $imgname;
+        // folder where images will be uploaded
+        $folder = 'assets/images/agencies/';
+        //function for saving the uploaded images in a specific folder
+        move_uploaded_file($filetmpname, $folder.$filename);
+        //inserting image details (ie image name) in the database
+
+         $product_image = $filename;
+      }
+
+
+      $this->db->
+      set('agency_name', $_POST['aname'])->
+      set('agency_image', $product_image)->
+      set('agency_description', $_POST['adescription'])->
+      set('delete_status', '0')->
+      insert('ci_gagencylist');
+
+		$this->session->set_userdata('swal', 'The agency has been added on the list.');
+		redirect('gagencylist');
+   }
+
+   public function edit_agency($id='')
+   {
+      $result = $this->db->
+      select('*')->
+      from('ci_gagencylist')->
+      where('gagency_id', $id)->
+      get()->result_array();
+
+      echo json_encode($result);
+      exit();
+   }
+
+
+  public function update_agency($pid=''){
+
+    $data1 = $_POST['edit-imagebase64'];
+
+    if($data1 == 'data:,'){
+      $this->db->
+      set('agency_name', $_POST['edit_aname'])->
+      set('agency_description', $_POST['edit_adescription'])->
+      where('gagency_id', $_POST['id_agency_id'])->
+      update('ci_gagencylist');
+    }else{
+       list($type, $data1) = explode(';', $data1);
+       list(, $data1)      = explode(',', $data1);
+       $data5 = base64_decode($data1);
+       $imgname = "agency_".md5(rand()).".png";
+       $test = file_put_contents("assets/images/agencies/$imgname", $data5);
+
+       $filename = $filetmpname = $imgname;
+       // folder where images will be uploaded
+       $folder = 'assets/images/agencies/';
+       //function for saving the uploaded images in a specific folder
+       move_uploaded_file($filetmpname, $folder.$filename);
+       //inserting image details (ie image name) in the database
+
+       $this->db->
+       set('agency_name', $_POST['edit_aname'])->
+       set('agency_image', $filename)->
+       set('agency_description', $_POST['edit_adescription'])->
+       where('gagency_id', $_POST['id_agency_id'])->
+       update('ci_gagencylist');
+    }
+
+    $this->session->set_userdata('swal', 'The agency has been updated on the list.');
+    redirect('gagencylist');
+  }
+
+  public function delete_agency($id=''){
+
+    $this->db->
+    set('delete_status', '1')->
+    where('gagency_id',$id)->
+    update('ci_gagencylist');
+
+    $this->session->set_userdata('swal','Agency has been removed from the list.');
+    redirect('gagencylist');
+  }
 
 }
--- application/modules/gagencylist/views/index.php
+++ application/modules/gagencylist/views/index.php
@@ -8,7 +8,9 @@
                <h3 class="text-themecolor page-title-text">Government Agency List</h3>
            </div>
            <div class="col-md-7 align-self-center text-right center-container">
-             <button type="button" class="btn btn-btn-mod blue-btn" style="color: white;" data-toggle="modal" data-target="#addAgency"><i class="fa fa-plus-circle"></i> Create New Agency</button>
+             <?php if($this->session->userdata('user_details')[0]['user_type'] == 'admin'){ ?>
+               <button type="button" class="btn btn-btn-mod blue-btn" style="color: white;" data-toggle="modal" data-target="#addAgency"><i class="fa fa-plus-circle"></i> Create New Agency</button>
+             <?php } ?>
          </div>
       </div>
       <div class="row">
@@ -34,6 +36,45 @@
       </div>
    </div>
 
+   <!-- View Modal -->
+   <div class="modal fade" id="viewAgency" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+     <div class="modal-dialog" role="document">
+       <div class="modal-content">
+         <div class="modal-header">
+           <h5 class="modal-title" id="addUserLabel">View Agency</h5>
+           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+             <span aria-hidden="true">&times;</span>
+           </button>
+         </div>
+        <div class="modal-body">
+           <div class="form-row">
+             <div class="form-group col-md-12">
+                <div style="width: 100%;">
+                  <div id="img-box" class="x_panel" style="text-align: center; border: none;">
+                      <img id="view-img" src="" alt="user picture" class="profile_img" style="margin-left: auto; max-width: 300px;">
+                         <!-- <input type="button" name="uploadpic" value="Upload" class="btn btn-primary hid1 upload-result"> -->
+                         <!-- <input type="button" name="cancel" value="Cancel" id="cancel" style="display: none;" class="btn btn-warning"> -->
+                      <!-- </form> -->
+                      <!-- <a class="btn btn-sm btn-danger remove_pic" style="margin: 10px 0px; background-color: #960722;" href="</?php echo base_url('productlist'); ?>/remove_picture/</?php echo $result[0]->user_id;?>">Remove Profile Picture</a> -->
+                  </div>
+               </div>
+            </div>
+           </div>
+           <div class="form-row">
+             <div class="form-group col-md-12" style="text-align: center;">
+               <span id="view_aname"></span>
+             </div>
+           </div>
+           <div class="form-row">
+              <div class="form-group col-md-12" style="text-align: justify; padding: 15px;">
+                  <span id="view_adescription"></span>
+              </div>
+           </div>
+        </div>
+       </div>
+     </div>
+   </div>
+
    <!-- Add Modal -->
    <div class="modal fade" id="addAgency" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
      <div class="modal-dialog" role="document">
@@ -44,30 +85,107 @@
              <span aria-hidden="true">&times;</span>
            </button>
          </div>
-         <form id="add_technician_form" action="<?php echo base_url(); ?>userlist/add_user" method="post">
+         <form id="add_agency_form" action="<?php echo base_url(); ?>gagencylist/add_agency" method="post" enctype="multipart/form-data">
             <div class="modal-body">
                <div class="form-row">
-                 <div class="form-group col-md-12" style="text-align: center;">
-                   <label for="image">Image</label>
-                   <input type="file" name="fileToUpload" class="form-control" id="image">
-                 </div>
+                 <div class="form-group col-md-12">
+                    <div style="width: 100%;">
+                      <div id="img-box" class="x_panel" style="text-align: center; border: none;">
+                          <div id="upload-demo" style="display: none;"></div>
+                          <img id="img" src="<?php echo base_url(); ?>assets/images/main-logo.png" alt="user picture" class="profile_img" style="margin-left: auto; max-width: 300px; margin-bottom:30px; border: 5px double #187abe;">
+
+                          <!-- <form id="imgform" action="</?php echo base_url('productlist'); ?>/add_product" method="post" enctype="multipart/form-data"> -->
+                             <input type="file" class="actionUpload inputpicture" name="pic" accept="image/*" id="picture" style="margin: 0 auto; display: none;">
+                             <label for="picture" class="choose-picture">Choose A Picture</label>
+                             <!-- <input type="file" name="pic" accept="image/*" onchange="readURL(this);" style="margin: 0 auto;"> -->
+                             <br>
+                             <input type="button" class="remove-picture inputpicture"  id="remove-picture" name="remove-picture" style="margin: 0 auto; display: none;">
+                             <label for="remove-picture" class="remove-pic" style="display: none;">Use default</label>
+                             <br>
+                             <input type="hidden" id="imagebase64" name="imagebase64">
+                             <!-- <input type="button" name="uploadpic" value="Upload" class="btn btn-primary hid1 upload-result"> -->
+                             <!-- <input type="button" name="cancel" value="Cancel" id="cancel" style="display: none;" class="btn btn-warning"> -->
+                          <!-- </form> -->
+                          <!-- <a class="btn btn-sm btn-danger remove_pic" style="margin: 10px 0px; background-color: #960722;" href="</?php echo base_url('productlist'); ?>/remove_picture/</?php echo $result[0]->user_id;?>">Remove Profile Picture</a> -->
+                      </div>
+                   </div>
+                </div>
                </div>
                <div class="form-row">
                  <div class="form-group col-md-12">
-                    <label for="address">Name</label>
-                   <input type="text" class="form-control" name="address" id="address" placeholder="Enter Agency Name" value="<?php if(isset($_POST['address'])){echo $_POST['address'];} ?>" required>
+                    <label for="aname">Agency Name</label>
+                   <input type="text" class="form-control" name="aname" id="aname" placeholder="Enter Agency Name" value="<?php if(isset($_POST['address'])){echo $_POST['address'];} ?>" required>
                  </div>
                </div>
                <div class="form-row">
                   <div class="form-group col-md-12">
-                     <label for="description">Description</label>
-                     <textarea type="text" class="form-control" name="description" id="description" placeholder="Enter Agency Description" style="width: 100%;" required></textarea>
+                     <label for="adescription">Description</label>
+                     <textarea type="text" class="form-control" name="adescription" id="adescription" placeholder="Enter Agency Description" style="width: 100%;" required></textarea>
                   </div>
                </div>
             </div>
             <div class="modal-footer">
                <span class="error_msg" style="color: red; font-size: 15px; margin-left: 0; width: 100%;"></span>
-               <button type="submit" class="btn btn-primary blue-btn">Submit</button>
+               <button type="submit" class="btn btn-primary blue-btn upload-result">Submit</button>
+            </div>
+         </form>
+       </div>
+     </div>
+   </div>
+
+   <!-- Edit Modal -->
+   <div class="modal fade" id="editAgency" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+     <div class="modal-dialog" role="document">
+       <div class="modal-content">
+         <div class="modal-header">
+           <h5 class="modal-title" id="addUserLabel">Edit Agency</h5>
+           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+             <span aria-hidden="true">&times;</span>
+           </button>
+         </div>
+         <form id="edit_agency_form" action="<?php echo base_url(); ?>gagencylist/update_agency" method="post" enctype="multipart/form-data">
+            <div class="modal-body">
+               <div class="form-row">
+                 <div class="form-group col-md-12">
+                   <input type="hidden" name="id_agency_id" value="">
+                    <div style="width: 100%;">
+                      <div id="edit-img-box" class="x_panel" style="text-align: center; border: none;">
+                          <div id="edit-upload-demo" style="display: none;"></div>
+                          <img id="edit-img" src="<?php echo base_url(); ?>assets/images/main-logo.png" alt="user picture" class="profile_img" style="margin-left: auto; max-width: 300px; margin-bottom:30px; border: 5px double #187abe;">
+
+                          <!-- <form id="imgform" action="</?php echo base_url('productlist'); ?>/add_product" method="post" enctype="multipart/form-data"> -->
+                             <input type="file" class="edit-actionUpload inputpicture" name="edit-pic" accept="image/*" id="edit-picture" style="margin: 0 auto; display: none;">
+                             <label for="edit-picture" class="edit-picture">Choose A Picture</label>
+                             <!-- <input type="file" name="pic" accept="image/*" onchange="readURL(this);" style="margin: 0 auto;"> -->
+                             <br>
+                             <input type="button" class="remove-picture inputpicture"  id="edit-remove-picture" name="edit-remove-picture" style="margin: 0 auto; display: none;">
+                             <label for="edit-remove-picture" class="edit-remove-pic" style="display: none;">Use default</label>
+                             <br>
+                             <input type="hidden" id="edit-imagebase64" name="edit-imagebase64">
+                             <!-- <input type="button" name="uploadpic" value="Upload" class="btn btn-primary hid1 upload-result"> -->
+                             <!-- <input type="button" name="cancel" value="Cancel" id="cancel" style="display: none;" class="btn btn-warning"> -->
+                          <!-- </form> -->
+                          <!-- <a class="btn btn-sm btn-danger remove_pic" style="margin: 10px 0px; background-color: #960722;" href="</?php echo base_url('productlist'); ?>/remove_picture/</?php echo $result[0]->user_id;?>">Remove Profile Picture</a> -->
+                      </div>
+                   </div>
+                </div>
+               </div>
+               <div class="form-row">
+                 <div class="form-group col-md-12">
+                    <label for="edit_aname">Agency Name</label>
+                   <input type="text" class="form-control" name="edit_aname" id="edit_aname" placeholder="Enter Agency Name" value="<?php if(isset($_POST['address'])){echo $_POST['address'];} ?>" required>
+                 </div>
+               </div>
+               <div class="form-row">
+                  <div class="form-group col-md-12">
+                     <label for="edit_adescription">Description</label>
+                     <textarea type="text" class="form-control" name="edit_adescription" id="edit_adescription" placeholder="Enter Agency Description" style="width: 100%;" required></textarea>
+                  </div>
+               </div>
+            </div>
+            <div class="modal-footer">
+               <span class="error_msg" style="color: red; font-size: 15px; margin-left: 0; width: 100%;"></span>
+               <button type="submit" class="btn btn-primary blue-btn edit-upload-result">Submit</button>
             </div>
          </form>
        </div>
